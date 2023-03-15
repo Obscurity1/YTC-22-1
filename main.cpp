a18606e29b0c4f42f875a0842d2e0c070101334e
Shasherin Viktor <114868521+Obscurity1@users.noreply.github.com>
2023-03-15 18:22:42 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,113 +1,78 @@
 #include <iostream>
-#include <array>
+#include <string>
+#include <algorithm>
+
 using namespace std;
-
-//№ 1. Переобразовать матрицу: элементы строки, в которой находится минимальный элемент матрицы, заменить нулями
-/*int main()
+/* №4
+int main()
 {
-    array<array<int, 4>, 4> a;
-    for(int i=0;i<4;i++)
-        for(int j=0;j<4;j++)
-            a[i][j]=rand()%10;
-
-    int  min_el_j, min_el;
-    for(int i=0;i<4;i++){
-        for(int j=0;j<6;j++){
-            if(a[i][j]<min_el){
-                min_el = a[i][j];
-                min_el_j = i;
-             }
-         }
+    string text;
+    string bword = "  ";
+    getline(cin, text);
+    int pos =0;
+    for (int i = 0; i < text.length(); i++) {
+        pos = text.find(bword);
+        if(pos == string::npos)
+            break;
+        text.erase(pos, 1);
     }
-    for(int i=0;i<4;i++){
-        a[min_el_j][i] = 0;
-    }
-
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++)
-            cout<<a[i][j]<<' ';
-    cout<<endl;
-
-    }
-}*/
-//№ 6. Дана матрица. Составить массив, каждый элемент которого равен максимальному элементу соотвветствующей строки матрицы
-/*int main(){
-    array<array<int, 3>, 3> num;
-    for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++)
-            num[i][j]=rand()%20;
-
-    array<int, 3> max={0,0,0};
-
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++)
-            if(num[i][j]>max[i])
-                max[i]=num[i][j];}
-
-   for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++)
-            num[i][j]=max[i];
-
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++)
-            cout<<num[i][j]<<' ';
-    cout<<endl;}
-}*/
-//№ 8. Дана матрица. Элементы первой строки - мощность электромотора, второй строки - категория матора(от 1 до 3). Найти самый мощный двигатель категории 3.
-/*int main(){
-    array<array<int, 10>, 2> mat;
-    array<int, 10> max;
-
-    for(int i=0;i<1;i++){
-        for(int j=0;j<10;j++)
-            mat[i][j]=rand()%100+20;}
-
-    for(int i=1;i<2;i++){
-        for(int j=0;j<10;j++)
-            mat[i][j]=rand()%3+1;}
-
-
-    for(int j=0;j<10;j++){
-        for(int i=0;i<2;i++)
-            cout<<mat[i][j]<<' ';
-    cout<<endl;}
-    cout<<endl;
-
-    for(int i=0;i<10;i++)
-        max[i]=0;
-
-    for(int j=0;j<10;j++)
-        if(mat[1][j]==3)
-            max[j]=mat[0][j];
-
-    int MAX=0;
-    for(int i=0;i<max.size();i++){
-            if(max[i]>MAX)
-                MAX=max[i];}
-    cout<<MAX<<endl;
-}*/
-//№ 9b.  Состояние робота на шахматной доске. Робот характеризуется уровнем заряда, скоростью движения.
-/*int main(){
-    int size;
-    cout<<"Vvedite kolichestvo robotov: ";
-    cin>>size;
-    double** matr=new double* [2];
-    for(int i=0;i<2;i++)
-        matr[i]=new double[size];
-
-    for(int i=0;i<size;i++){
-        cout<<"Vvedite zaryd "<<i+1<<" robota: ";
-        cin>>matr[0][i];
-        cout<<"Vvedite skorost "<<i+1<<" robota: ";
-        cin>>matr[1][i];}
-
-    for(int i=0;i<2;i++){
-        for(int j=0;j<size;j++)
-            cout<<matr[i][j]<<' ';
-    cout<<endl;}
-
-    for(int i=0;i<2;i++)
-        delete[] matr[i];
-    delete[] matr;
+    cout<<text;
     return 0;
 }*/
+/* №8
+int main(){
+    string text;
+    string chars;
+    getline(cin, text);
+    getline(cin, chars);
+    for(char c: chars){
+        text.erase(remove(text.begin(), text.end(), c), text.end());
+    }
+    cout<<text<<endl;
+}*/
+/* №17
+int main(){
+    const int size = 5;
+        array<string, size> arr;
+           // заполнение массива
+        for (int i = 0; i < size; i++) {
+            cin >> arr[i];
+        }
+
+        string temp; // временная переменная для обмена элементов местами
+
+        // Сортировка массива пузырьком
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j].length() > arr[j + 1].length()) {
+                    // меняем элементы местами
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Вывод отсортированного массива на экран
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+}*/
+// №22
+int main(){
+    string str, d, s;
+    int a, b;
+        getline(cin, str);
+        a=str.find("/*");
+        b=str.rfind("*/");
+
+        for(int i=0;i<a;i++)
+            d+=str[i];
+        for(int i=0;i<d.length()-1;i++)
+            cout<<d[i];
+        for(int i=str.length();i>b;i--)
+            s+=str[i];
+        for(int i=s.length()-1; i>=0;i--)
+            cout<<s[i];
+}
